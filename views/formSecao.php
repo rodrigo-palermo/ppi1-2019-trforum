31ca8930e626938e64e67423027d2e50e55378da
Rodrigo Palermo <avilapalermo@gmail.com>
2019-07-13 02:47:22 -0300

    Corrections on forms.

--- views/formSecao.php
+++ views/formSecao.php
@@ -1,25 +1,29 @@
 <h2>Seção</h2>
-    <form id="formSecao" action=''> <!--method='post'/-->
-        <div class="form-group">
-            <label for="id">Id</label>
-            <input type="number" id="id" name="id" value="<?= isset($_GET['id'])?$object->getId():''; ?>" readonly>
-            <br/>
-            <label for="ordem">Ordem</label>
-            <input type="number" id="ordem" name="ordem" value="<?= isset($_GET['id'])?$object->getOrdem():''; ?>">
-            <br/>
-            <label for="nome">Nome</label>
-            <input type="text" id="nome" name="nome" value="<?= isset($_GET['id'])?$object->getNome():''; ?>">
-            <br/>
-            <label for="descricao">Descrição</label>
-            <textarea class="for-control" rows="5" cols="100" id="descricao" name="descricao"><?=isset($_GET['id'])? $object->getDescricao():'';?></textarea>
-            <!--textarea deve ficar na mesma linha para evitar espaços na frente do texto ao criar/editar-->
-            <!--/* isset($_GET['id'])? addslashes(str_replace(PHP_EOL, '', $object->getDescricao())):'';*/-->
-        </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Salvar">
-            <input type="text" name="<?= isset($_GET['id'])?'submitEditar':'submitCriar';?>" hidden>
-        </div>
-    </form>
+<form id="formSecao" action=''> <!--method='post'/-->
+    <div class="form-group">
+        <label for="id">Id</label>
+        <input type="number" class="form-control" id="id" name="id" value="<?= isset($_GET['id'])?$object->getId():''; ?>" readonly>
+    </div>
+    <div class="form-group">
+        <label for="ordem">Ordem</label>
+        <input type="number" class="form-control" id="ordem" name="ordem" value="<?= isset($_GET['id'])?$object->getOrdem():''; ?>">
+    </div>
+    <div class="form-group">
+        <label for="nome">Nome</label>
+        <input type="text" class="form-control" id="nome" name="nome" value="<?= isset($_GET['id'])?$object->getNome():''; ?>">
+    </div>
+    <div class="form-group">
+        <label for="descricao">Descrição</label>
+        <textarea class="form-control" rows="5" cols="100" id="descricao" name="descricao"><?=isset($_GET['id'])? $object->getDescricao():'';?></textarea>
+        <!--textarea deve ficar na mesma linha para evitar espaços na frente do texto ao criar/editar-->
+        <!--/* isset($_GET['id'])? addslashes(str_replace(PHP_EOL, '', $object->getDescricao())):'';*/-->
+    </div>
+    </div>
+    <div class="form-group">
+        <input type="submit" class="btn btn-primary" value="Salvar">
+        <input type="text" name="<?= isset($_GET['id'])?'submitEditar':'submitCriar';?>" hidden>
+    </div>
+</form>
 
 <script>
 
--- views/paginaAdminDB.php
+++ views/paginaAdminDB.php
@@ -1,63 +1,75 @@
 <h2>Banco de Dados</h2>
 
-<h3>Seção</h3>
+<ul class="nav nav-tabs">
+    <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#secao">Seção</a></li>
+    <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#forum">Fórum</a></li>
+</ul>
 
-<button type="button" class="btn btn-primary" onclick='acaoAdm(`secao`, `criar`)'>Novo</button>
-<table class="table">
-    <thead>
-    <tr><th>Id</th>
-        <th>Ordem</th>
-        <th>Nome</th>
-        <th>Descrição</th>
-        <th>Ações</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach ($arrSecao as $secao) {
-        $rowData =  "<tr>".PHP_EOL;
-        $rowData .= "<td>".$secao->getId()."</td>".PHP_EOL;
-        $rowData .= "<td>".$secao->getOrdem()."</td>".PHP_EOL;
-        $rowData .= "<td>".$secao->getNome()."</td>".PHP_EOL;
-        $rowData .= "<td>".$secao->getDescricao()."</td>".PHP_EOL;
-        $rowData .= "<td><div class='btn-group'>".PHP_EOL;
-        $rowData .= "<button type='button' class='btn btn-primary' onclick='acaoAdm(`secao`, `editar&id=".$secao->getId()."`)'>Editar</button>";
-        $rowData .= "<br><br><button type='button' class='btn btn-danger' onclick='acaoAdm(`secao`, `excluir`)'>Excluir</button>".PHP_EOL;
-        $rowData .= "</div></td>".PHP_EOL;
-        $rowData .= "</tr>".PHP_EOL;
-        print $rowData;
-    } ?>
+<div class="tab-content">
+    <div id="secao" class="container tab-pane active">
+        <h3>Seção</h3>
 
-    </tbody>
-</table>
+        <button type="button" class="btn btn-primary" onclick='acaoAdm(`secao`, `criar`)'>+</button>
+        <table class="table">
+            <thead>
+            <tr><th>Id</th>
+                <th>Ordem</th>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Ações</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($arrSecao as $secao) {
+                $rowData =  "<tr>".PHP_EOL;
+                $rowData .= "<td>".$secao->getId()."</td>".PHP_EOL;
+                $rowData .= "<td>".$secao->getOrdem()."</td>".PHP_EOL;
+                $rowData .= "<td>".$secao->getNome()."</td>".PHP_EOL;
+                $rowData .= "<td>".$secao->getDescricao()."</td>".PHP_EOL;
+                $rowData .= "<td><div class='btn-group'>".PHP_EOL;
+                $rowData .= "<button type='button' class='btn btn-primary' onclick='acaoAdm(`secao`, `editar&id=".$secao->getId()."`)'>Editar</button>";
+                $rowData .= "<br><br><button type='button' class='btn btn-danger' onclick='acaoAdm(`secao`, `excluir`)'>Excluir</button>".PHP_EOL;
+                $rowData .= "</div></td>".PHP_EOL;
+                $rowData .= "</tr>".PHP_EOL;
+                print $rowData;
+            } ?>
 
-<h3>Fórum</h3>
+            </tbody>
+        </table>
+    </div>
 
-<table class="table">
-    <thead>
-    <tr><th>Id</th>
-        <th>Id da Seção - colocar o nome da Seção</th>
-        <th>Nome</th>
-        <th>Descrição</th>
-        <th>Ações</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach ($arrForum as $forum) {
-        $rowData =  "<tr>".PHP_EOL;
-        $rowData .= "<td>".$forum->getId()."</td>".PHP_EOL;
-        $rowData .= "<td>".$forum->getIdSecao()."</td>".PHP_EOL;
-        $rowData .= "<td>".$forum->getNome()."</td>".PHP_EOL;
-        $rowData .= "<td>".$forum->getDescricao()."</td>".PHP_EOL;
-        $rowData .= "<td><div class='btn-group'>".PHP_EOL;
-        $rowData .= "<button type='button' class='btn btn-primary' onclick='acaoAdm(`forum`, `editar&id=".$forum->getId()."`)'>Editar</button>";
-        //todo: implementar views e controllers forum
-        $rowData .= "<br><br><button type='button' class='btn btn-danger' onclick='acaoAdm(`forum`, `excluir`)'>Excluir</button>".PHP_EOL;
-        $rowData .= "</div></td>".PHP_EOL;
-        $rowData .= "</tr>".PHP_EOL;
-        print $rowData;
-    } ?>
+    <div id="forum" class="container tab-pane fade" >
+        <h3>Fórum</h3>
 
-    </tbody>
-</table>
+        <button type="button" class="btn btn-primary" onclick='acaoAdm(`forum`, `criar`)'>+</button>
+        <table class="table">
+            <thead>
+            <tr><th>Id</th>
+                <th>Id da Seção - colocar o nome da Seção</th>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Ações</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($arrForum as $forum) {
+                $rowData =  "<tr>".PHP_EOL;
+                $rowData .= "<td>".$forum->getId()."</td>".PHP_EOL;
+                $rowData .= "<td>".$forum->getIdSecao()."</td>".PHP_EOL;
+                $rowData .= "<td>".$forum->getNome()."</td>".PHP_EOL;
+                $rowData .= "<td>".$forum->getDescricao()."</td>".PHP_EOL;
+                $rowData .= "<td><div class='btn-group'>".PHP_EOL;
+                $rowData .= "<button type='button' class='btn btn-primary' onclick='acaoAdm(`forum`, `editar&id=".$forum->getId()."`)'>Editar</button>";
+                //todo: implementar views e controllers forum
+                $rowData .= "<br><br><button type='button' class='btn btn-danger' onclick='acaoAdm(`forum`, `excluir`)'>Excluir</button>".PHP_EOL;
+                $rowData .= "</div></td>".PHP_EOL;
+                $rowData .= "</tr>".PHP_EOL;
+                print $rowData;
+            } ?>
+
+            </tbody>
+        </table>
+    </div>
+</div>
